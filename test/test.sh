62dca14826945cb80a3d64ea7d1b9c88485f427a
Karl Fischer <fishi0x01@users.noreply.github.com>
2017-07-26 10:42:08 +0200

    Create local directory if missing on read (#24)

--- test/test.sh
+++ test/test.sh
@@ -10,12 +10,12 @@ bin/sdl -b testB -f ./test/test.config.yml read prod
 ##############
 # Check result
 #
-if ! diff -q test/qa-foo test/prod-foo &>/dev/null; then
+if ! diff -q test/qa-foo test/create/prod-foo &>/dev/null; then
    echo "Test failed!"
    exit 1
 fi
 
-if ! diff -q test/qa-bar test/prod-bar &>/dev/null; then
+if ! diff -q test/qa-bar test/create2/prod-bar &>/dev/null; then
    echo "Test failed!"
    exit 1
 fi
